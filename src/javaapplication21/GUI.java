dadbb132a6109ec8eb1e0905dfb7e53582c4514f
Basem Elsayed <ebasem65@gmail.com>
2018-05-16 00:27:01 +0200

    AiGameWords
    Ai game with prolog and GUI (JPL-Java)

---
+++ src/javaapplication21/GUI.java
@@ -0,0 +1,21 @@
+
+package javaapplication21;
+
+import org.jpl7.Query;
+
+
+public class GUI {
+
+    public static void main(String[] args) {
+        
+        new NewJFrame().setVisible(true);
+        
+        /*String s = "consult('C:/Users/Basem Elsayed/Desktop/20150071_20150080/20150071&20150080.pl')";
+        Query q1 = new Query(s);
+        System.out.println( "consult " + (q1.hasSolution() ? "succeeded" : "failed"));
+
+        q1 = new Query("test");
+        System.out.println( "consult " + (q1.hasSolution() ? "succeeded" : "failed"));*/
+    }
+    
+}
